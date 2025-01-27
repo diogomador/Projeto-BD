05c12e7125a3b31733fe28a69e7f01ec025711eb
Nego Nilso <137554033+skonn3r@users.noreply.github.com>
2025-01-26 23:50:49 -0300

    Delete Projeto-BD directory

--- Projeto-BD/models/config.py
+++
@@ -1,8 +0,0 @@
-class Config:
-    SECRET_KEY = 'SUPERDIFICIL'
-    
-    # Configurações do banco de dados MySQL
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'kauan123'
-    MYSQL_DB = 'db_biblioteca'
