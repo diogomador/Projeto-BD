536b3e5e133753dee2b520c5b3d03ae393e47fd2
EoTavinho <149340850+EoTavinho@users.noreply.github.com>
2025-01-29 11:17:31 -0300

    Ajustes no css dos filtros

--- models/config.py
+++ models/config.py
@@ -4,5 +4,5 @@ class Config:
     # Configurações do banco de dados MySQL
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = ''
+    MYSQL_PASSWORD = '190406'
     MYSQL_DB = 'db_biblioteca'
